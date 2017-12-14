0c5994b9784956c49f3f50ac0893a53f5b808b68
Aleksandar Hristovski <xrristo@gmail.com>
2017-12-14 18:27:40 +0100

    user model

--- includes/database.php
+++ includes/database.php
@@ -5,15 +5,19 @@
 	class MySQLDatabase {
 
 		private $connection;
-
+		public $last_query;
+		private $magic_quotes_active;
+		private $real_escape_string_exists;
 		public function __construct(){
 			$this->open_connection();
+			$this->magic_quotes_active = get_magic_quotes_gpc();
+			$this->real_escape_string_exists = function_exists("mysqli_real_escape_string"); // PHP >= v4.3.0
 		}
 
 		public function open_connection(){
 			$this->connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS);
 			if(!$this->connection){
-				die("Database connection failed: " . mysqli_error($this->connection));
+				die("Database connection failed: " . mysqli_connect_error($this->connection));
 			} else {
 				$db_select = mysqli_select_db($this->connection, DB_NAME);
 				if(!$db_select)
@@ -29,26 +33,29 @@
 		}
 
 		public function query($sql){
+			$this->last_query = $sql;
 			$result = mysqli_query($this->connection, $sql);
 			$this->confirm_query($result);
 			return $result;
 		}
 
 		private function confirm_query($result){
-			if(!$result)
-				die('Database query failed: ' . mysqli_error($this->connection));
+			if(!$result){
+				$output = "Database query failed: " . mysqli_error($this->connection) . "<br><br>";
+				$output .= "Last SQL Query: " . $this->last_query;
+				die($output);
+			}
 		}
 
-		public function mysql_prep($value){
-			$magic_quotes_active = get_magic_quotes_gpc();
-			$new_enough_php = function_exists("mysqli_real_escape_string"); // PHP >= v4.3.0
-			if($new_enough_php){
+		public function escape_value($value){
+			
+			if($this->real_escape_string_exists){
 				// echo "<br>new enough php<br>";
-				if($magic_quotes_active){
-					// $value = stripslashes($value);
+				if($this->magic_quotes_active){
+					$value = stripslashes($value);
 					// echo "magic quotes active<br>";
 				} else {
-					if(!$magic_quotes_active){
+					if(!$this->magic_quotes_active){
 						// echo "<br>magic quotes not active<br>";
 						$value = addslashes($value);
 					}
@@ -58,6 +65,24 @@
 			// die();
 			return $value;
 		}
+
+		public function fetch_array($result_set){
+			return mysqli_fetch_array($result_set);
+		}
+
+		public function num_rows($result_set){
+			return mysqli_num_rows($result_set);
+		}
+
+		public function insert_id(){
+			// get last id inserted in mysql db
+			return mysqli_insert_id();
+		}
+
+		public function affected_rows(){
+			return mysqli_affected_rows($this->connection);
+		}
+
 	}
 
 	$database = new MySQLDatabase();
--- public/index.php
+++ public/index.php
@@ -1,17 +1,35 @@
 <?php 
 
 	require_once('../includes/database.php');
-	echo isset($database) ? 'true' : 'false';
-	echo '<br>';
-	echo $database->mysql_prep("It's working?<br>");
+	require_once('../includes/user.php');
 
-	$sql = "INSERT INTO users (id, username,
-				password, first_name, last_name) VALUES (1, 'kskoglund', 'secretpwd', 'Kevin','Skoglund'); ";
+	// echo isset($database) ? 'true' : 'false';
+	// echo '<br>';
+	// echo $database->escape_value("It's working?<br>");
+
+	// $sql = "INSERT INTO users (id, username,
+	// 			password, first_name, last_name) VALUES (1, 'kskoglund', 'secretpwd', 'Kevin','Skoglund'); ";
 	
-	$result = $database->query($sql);
+	// $result = $database->query($sql);
 
-	$sql = "SELECT * FROM users WHERE id = 1";
-	$result_set = $database->query($sql);
-	$found_user = mysqli_fetch_array($result_set);
-	echo $found_user['username'];
+	// $sql = "SELECT * FROM users WHERE id = 1";
+	// $result_set = $database->query($sql);
+	// $found_user = $database->fetch_array($result_set);
+	// echo $found_user['username'];
+
+	// echo '<hr>';
+
+	$user = User::find_by_id(1);
+	
+	echo $user->full_name();
+	// echo $record['username'];
+
+	echo '<hr>';
+
+	$users = User::find_all();
+	foreach($users as $user) {
+		echo 'User: ' . $user->username . '<br>';
+		echo 'Name: ' . $user->full_name() . '<br><br>';
+	}
+
 ?>
